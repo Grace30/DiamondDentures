daf0043897ae383bc56d1b41625721118981847d
Jesús <Jess@JLEON>
2017-11-23 16:00:20 -0700

    Boton perron

--- DiseñoFinal/Almacen.designer.cs
+++ DiseñoFinal/Almacen.designer.cs
@@ -35,6 +35,7 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
             this.pbMat = new System.Windows.Forms.PictureBox();
             this.pbReq = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -52,7 +53,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button9 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAlmacen)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbMat)).BeginInit();
@@ -131,27 +131,37 @@
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Location = new System.Drawing.Point(21, 367);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(784, 78);
+            this.groupBox1.Size = new System.Drawing.Size(784, 96);
             this.groupBox1.TabIndex = 65;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(188, 18);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(84, 38);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Requisiciones por Proveedor";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // pbMat
             // 
             this.pbMat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pbMat.Location = new System.Drawing.Point(725, 54);
             this.pbMat.Name = "pbMat";
-            this.pbMat.Size = new System.Drawing.Size(25, 25);
+            this.pbMat.Size = new System.Drawing.Size(40, 40);
             this.pbMat.TabIndex = 8;
             this.pbMat.TabStop = false;
             // 
             // pbReq
             // 
             this.pbReq.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pbReq.Location = new System.Drawing.Point(633, 54);
+            this.pbReq.Location = new System.Drawing.Point(625, 54);
             this.pbReq.Name = "pbReq";
-            this.pbReq.Size = new System.Drawing.Size(25, 25);
+            this.pbReq.Size = new System.Drawing.Size(40, 40);
             this.pbReq.TabIndex = 8;
             this.pbReq.TabStop = false;
             // 
@@ -299,21 +309,11 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(188, 18);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(84, 38);
-            this.button9.TabIndex = 9;
-            this.button9.Text = "Requisiciones por Proveedor";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
-            // 
             // Almacen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(907, 452);
+            this.ClientSize = new System.Drawing.Size(907, 469);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
